2ef8c118110de0f5048c726a50abfd992ce88eb3
Sarbeng <ksarbengbaafi>
2026-04-28 09:05:48 +0000

    models created and connections established

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
 {
-    //
+    protected $connection = 'hr';
+
+    protected $table = 'staff';
+
+    public $timestamps = false;
+
+    public function save(array $options = []): void
+    {
+        // Throw an error if anything tries to save to this database
+        throw new \Exception('The HR database is read-only.');
+    }
 }
--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Unit extends Model
 {
-    //
+    protected $connection = 'hr';
+
+    protected $table = 'unit';
+
+    public $timestamps = false;
+
+    public function save(array $options = []): void
+    {
+        // Throw an error if anything tries to save to this database
+        throw new \Exception('The HR database is read-only.');
+    }
 }
