8010a4f1e132069bd1d29a004c9949d1dded1b85
Sarbeng <ksarbengbaafi>
2026-04-28 13:53:40 +0000

    member controller

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -170,11 +170,7 @@ class MemberController extends Controller
         if (! $loan) {
             return redirect()->route('loan-application')->with('error', 'Failed to submit loan application. Please try again.');
         } else {
-<<<<<<< HEAD
-            Log::info('Loan application created: '.$loan->application_ref.' for User ID: '.$loan->user_id);
-=======
             Log::info('Loan application created: '.$loan->application_ref.' for member ID: '.$loan->member_id);
->>>>>>> 13d10d3 (Changes made)
         }
 
         return redirect()->route('loan-application')->with('success', 'Loan application submitted! Ref: '.$loan->application_ref.' Confirmation email sent.');
