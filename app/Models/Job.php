f3698622cbc2b7f32de66e792f0453351d0f6e9d
Sarbeng <ksarbengbaafi>
2026-04-28 09:05:48 +0000

    models created and connections established

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    protected $connection = 'hr';
+
+    protected $table = 'job';
+
+    public $timestamps = false;
+
+    public function save(array $options = []): void
+    {
+        // Throw an error if anything tries to save to this database
+        throw new \Exception('The HR database is read-only.');
+    }
 }
